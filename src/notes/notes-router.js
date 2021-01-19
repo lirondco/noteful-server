81b795d91b6a66c312d30724023fe8f43bfe5fb9
Liron de Castro <liron.dco@gmail.com>
2021-01-19 00:49:49 -0800

    first actual commit

---
+++ src/notes/notes-router.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const xss = require('xss')
+const NotesService = require('./notes-service')
+const notesRouter = express.Router()
+const path = require('path')
+
+
+const jsonParser = express.json()
+
+const serializeNote = note => ({
+  id: note.id,
+  name: xss(note.name),
+  modified: note.modified, 
+  folderId: note.folderId,
+  content: xss(note.content),
+})
+
+notesRouter
+  .route('/')
+  .get((req, res, next) => {
+    const knexInstance = req.app.get('db')
+    NotesService.getAllNotes(knexInstance)
+      .then(notes => {
+        res.json(notes.map(serializeNote))
+      })
+      .catch(next)
+  })
+  .post(jsonParser, (req, res, next) => {
+    const { name, folderId, content } = req.body
+    const newNote = { name, content }
+
+    for (const [key, value] of Object.entries(newNote))
+      if (value == null)
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` }
+        })
+    
+    newNote.folderId = folderId
+    NotesService.insertNote(
+      req.app.get('db'),
+      newNote
+    )
+      .then(note => {
+        res
+          .status(201)
+          .location(path.posix.join(req.originalUrl, `/${note.id}`))
+          .json(serializeNote(note))
+      })
+      .catch(next)
+  })
+
+notesRouter
+  .route('/:note_id')
+  .all((req, res, next) => {
+    NotesService.getById(
+      req.app.get('db'),
+      req.params.note_id
+    )
+      .then(article => {
+        if (!article) {
+          return res.status(404).json({
+            error: { message: `Article doesn't exist` }
+          })
+        }
+        res.article = article
+        next()
+      })
+      .catch(next)
+  })
+  .get((req, res, next) => {
+    res.json(serializeArticle(res.article))
+  })
+  .delete((req, res, next) => {
+    ArticlesService.deleteArticle(
+      req.app.get('db'),
+      req.params.article_id
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+  .patch(jsonParser, (req, res, next) => {
+    const { title, content, style } = req.body
+    const articleToUpdate = { title, content, style }
+
+    const numberOfValues = Object.values(articleToUpdate).filter(Boolean).length
+    if (numberOfValues === 0) {
+      return res.status(400).json({
+        error:{
+          message: `Request body must contain either 'title', 'style', or 'content'`
+        }
+      })
+    }
+
+    ArticlesService.updateArticle(
+      req.app.get('db'),
+      req.params.article_id,
+      articleToUpdate
+    )
+      .then(numRowsAffected => {
+        res.status(204).end()
+      })
+      .catch(next)
+  })
+
+module.exports = articlesRouter
