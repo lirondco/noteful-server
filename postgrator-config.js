a451fb3c422dc2e2dcfe13d6775697cb0d7dadf4
Liron de Castro <liron.dco@gmail.com>
2021-01-20 13:22:53 -0800

    push to heroku

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-  "migrationsDirectory": "migrations",
+  "migrationDirectory": "migrations",
   "ssl": !!process.env.SSL,
   "driver": "pg",
   "connectionString": (process.env.NODE_ENV === 'test')
