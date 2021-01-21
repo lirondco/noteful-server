4cb5018de50eb74877e3ab1681cacd6b6196a19c
Liron de Castro <liron.dco@gmail.com>
2021-01-21 07:43:15 -0800

    more polishing of config files

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,5 +3,10 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString":  "dbname=d5bmonrep5nmap host=ec2-52-72-190-41.compute-1.amazonaws.com port=5432 user=pdgquyfyixqyxw password=1f4faab63c7cf0338291d3e5dd38d77a14227dffd2a76ee385ee066bbe58e9e5 sslmode=require"
+  "connectionString": (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    }
 }
